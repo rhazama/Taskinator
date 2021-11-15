ca5505635fb7e64edeff89d5219d2e58c1bca6c1
Ryan Hazama <ryanhazama@Ryans-Laptop.local>
2021-11-14 19:02:26 -0800

    replaced to formEl and added event.predefault()

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 buttonEl.addEventListener("click", function() {
@@ -8,11 +8,13 @@ buttonEl.addEventListener("click", function() {
   tasksToDoEl.appendChild(listItemEl);
 });
 
-var createTaskHandler = function() {
+var createTaskHandler = function(event) {
+    event.preventDefault();
+
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
     tasksToDoEl.appendChild(listItemEl);
   }
 
-  buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
